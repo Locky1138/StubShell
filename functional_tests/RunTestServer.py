537261feac3885fd927f8ad1d80e84ae1267f8c9
Locky <locky1138@gmail.com>
2015-08-20 11:52:43 -0500

    REFACTOR: functional test server now imports tests.exe_config

--- functional_tests/RunTestServer.py
+++ functional_tests/RunTestServer.py
@@ -12,10 +12,7 @@ from twisted.internet import reactor
 def main():
     users = {'usr': 'pas'}
     executables = get_executables('executables/special_commands.py')
-
-    #Ugly patch to get FT working, should be loded with -c for FT's
-    from tests.exe_config import exe_wait
-    executables.append(exe_wait)
+    executables = get_executables('tests/exe_config.py')
 
     log.startLogging(sys.stderr)
 
